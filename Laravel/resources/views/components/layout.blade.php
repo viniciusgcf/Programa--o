0c0ee0ff2bc8168925beb6a9b34e07dfcae532d6
Vinicius Gabriel <viniciusgabrielcf@gmail.com>
2024-06-14 12:37:17 -0300

    update

--- Laravel/resources/views/components/layout.blade.php
+++ Laravel/resources/views/components/layout.blade.php
@@ -12,8 +12,13 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ route('series.index') }}">Home</a>
+        @auth
+        <a href="{{ route('logout') }}">Sair</a>    
+        @endauth
 
-        <a href="{{ route('logout') }}">Sair</a>
+        @guest
+        <a href="{{ route('login') }}">Entrar</a>  
+        @endguest
     </div>
 </nav>
 <div class="container">
