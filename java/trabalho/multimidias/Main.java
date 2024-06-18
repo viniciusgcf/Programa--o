540c1228ed470fe396c4cb60b40760a3b9c58101
Vinícius Gabriel <149544283+viniciusgcf@users.noreply.github.com>
2024-06-18 07:58:28 -0300

    Add files via upload

---
+++ java/trabalho/multimidias/Main.java
@@ -0,0 +1,33 @@
+package multimidias;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        Text text = new Text();
+        text.setText("Bem vindo ao aplicativo de vídeo aulas, selecione a aula que deseja realizar: ");
+        text.setX(50);
+        text.setY(50);
+
+        stage.setFullScreen(false);
+        stage.setTitle("Trabalho de Multimídias");
+        stage.setScene(scene);
+        stage.show();
+        // dasda
+    }
+
+}
