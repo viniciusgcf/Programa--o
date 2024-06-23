c34a67856432b362c459e90647dea7eeb353cafd
viniciusgcf note <viniciusgabrielcf@gmail.com>
2024-06-23 17:39:27 -0300

    update

---
+++ java/multimidias/Main.java
@@ -0,0 +1,15 @@
+package multimidias;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+}
