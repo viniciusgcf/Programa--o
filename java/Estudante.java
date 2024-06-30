cdc5490f0be9972501f5e0ec22d14584b45120ce
viniciusgcf note <viniciusgabrielcf@gmail.com>
2024-06-30 16:15:09 -0300

    update

--- java/Estudante.java
+++ java/Estudante.java
@@ -3,6 +3,25 @@ public class Estudante extends Pessoa {
     private float nota1;
     private float nota2;
 
+    public Estudante(String nome, String cpf, String matricula, float nota1, float nota2) {
+        super(nome, cpf);
+        this.matricula = matricula;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNota1(float nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(float nota2) {
+        this.nota2 = nota2;
+    }
+
     public String getMatricula() {
         return matricula;
     }
@@ -20,11 +39,18 @@ public class Estudante extends Pessoa {
     }
 
     public String getEstudanteCSV() {
-        return "a";
+        return String.join(";", getNome(), getCpf(), getMatricula(), Double.toString(nota1), Double.toString(nota2));
     }
 
     public void setEstudanteCSV(String linha) {
-
+        String[] Linha = linha.split(";");
+        if (Linha.length == 5) {
+            setNome(Linha[0]);
+            setCpf(Linha[1]);
+            setMatricula(Linha[2]);
+            Linha[3] = Double.toString(nota1);
+            Linha[4] = Double.toString(nota2);
+        }
     }
 
 }
--- java/Pessoa.java
+++ java/Pessoa.java
@@ -2,12 +2,23 @@ public class Pessoa {
     private String nome;
     private String cpf;
 
+    public Pessoa(String nome2, String cpf2) {
+    }
+
     public String getNome() {
         return nome;
     }
 
     public String getCpf() {
         return cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
 }
