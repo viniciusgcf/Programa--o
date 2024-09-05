675853af1928147322de6aaf9237de225ca318fb
Vinicius Gabriel <viniciusgabrielcf@gmail.com>
2024-09-05 12:54:01 -0300

    updatye

--- laravel-ACL/database/seeders/DatabaseSeeder.php
+++ laravel-ACL/database/seeders/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run() : void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
