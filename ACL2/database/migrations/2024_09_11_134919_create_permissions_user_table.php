648cf4f62deae709ecf3ad1a09ca71c01b2cd19f
Vinicius Gabriel <viniciusgabrielcf@gmail.com>
2024-09-11 11:18:44 -0300

    .

---
+++ ACL2/database/migrations/2024_09_11_134919_create_permissions_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Permission;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up() : void
+    {
+        Schema::create('permissions_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Permission::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down() : void
+    {
+        Schema::dropIfExists('permissions_user');
+    }
+};
