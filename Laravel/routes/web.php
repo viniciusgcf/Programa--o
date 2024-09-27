450c86de7145aeaad8dcd4e2bb169f1810400db5
Vinicius Gabriel <viniciusgabrielcf@gmail.com>
2024-09-27 10:52:16 -0300

    .

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,23 +1,28 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
 Route::get('/', function () {
 
-    Auth::loginUsingId(2);
+    Auth::loginUsingId(1);
 
     $users = User::all();
 
     return view('welcome', ['users' => $users]);
 });
 
-Route::get('/user/create', function () {
-
-    return view('user_create');
-});
-
 Route::get('/login', function () {
 
     var_dump('login view');
 })->middleware('can:login_page');
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'index')->name('login.index');
+    Route::post('/login', 'store')->name('login.store');
+    Route::get('/logout', 'destroy')->name('login.destroy');
+});
