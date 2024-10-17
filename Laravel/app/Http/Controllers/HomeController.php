09490456bc1437e98225b236fe16fde924016ecb
Vinicius Gabriel <viniciusgabrielcf@gmail.com>
2024-10-17 13:01:36 -0300

    a

--- Laravel/app/Http/Controllers/HomeController.php
+++ Laravel/app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $users = User::all();
+        return view('home', ['users' => $users]);
     }
 }
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -5,22 +5,21 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
-Route::get('/', function () {
+Auth::loginUsingId(1);
 
-    $users = User::all();
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-    return view('welcome', ['users' => $users]);
-});
+Route::get('/teste', function () {
+    var_dump('login view');
+})->middleware('can:login_page');
 
-// Route::get('/login', function () {
-
-//     var_dump('login view');
-// })->middleware('can:login_page');
-
-Route::get('/master', [HomeController::class, 'index'])->name('home');
+// Route::get('/master', [HomeController::class, 'index'])->name('home');
 
 Route::controller(LoginController::class)->group(function () {
     Route::get('/login', 'index')->name('login.index');
     Route::post('/login', 'store')->name('login.store');
     Route::get('/logout', 'destroy')->name('login.destroy');
 });
+
+
+
