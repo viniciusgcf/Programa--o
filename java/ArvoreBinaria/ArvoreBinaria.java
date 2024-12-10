f767cfd796bbe52cf3d1d600b6f6b2561eb482b6
Vinicius Gabriel <viniciusgabrielcf@gmail.com>
2024-12-10 12:01:30 -0300

    a

--- java/ArvoreBinaria/ArvoreBinaria.java
+++ java/ArvoreBinaria/ArvoreBinaria.java
@@ -1,11 +1,78 @@
 package ArvoreBinaria;
 
+class Nodo {
+    int dados;
+    Nodo esq;
+    Nodo dir;
+
+    public Nodo(int dados) {
+        this.dados = dados;
+    }
+}
+
 public class ArvoreBinaria {
 
-    Nodo nodo;
+    Nodo raiz;
 
     public void add(int dados) {
-        nodo = new Nodo(dados);
+        raiz = adicionar(raiz, dados);
     }
 
+    public Nodo adicionar(Nodo raiz, int dados) {
+        if (raiz == null) {
+            raiz = new Nodo(dados);
+        } else if (dados < raiz.dados) {
+            raiz.esq = adicionar(raiz.esq, dados);
+        } else if (dados > raiz.dados) {
+            raiz.dir = adicionar(raiz.dir, dados);
+        }
+        return raiz;
+    }
+
+    public void preOrdemInicio() {
+        preOrdem(raiz);
+    }
+
+    public void preOrdem(Nodo raiz) {
+        if (raiz != null) {
+            System.out.print(raiz.dados + " ");
+            preOrdem(raiz.esq);
+            preOrdem(raiz.dir);
+        }
+    }
+
+    public void emOrdemInicio() {
+        emOrdem(raiz);
+    }
+
+    public void emOrdem(Nodo raiz) {
+        if (raiz != null) {
+            emOrdem(raiz.esq);
+            System.out.print(raiz.dados + " ");
+            emOrdem(raiz.dir);
+        }
+    }
+
+    public void posOrdemInicio() {
+        posOrdem(raiz);
+    }
+
+    public void posOrdem(Nodo raiz) {
+        if (raiz != null) {
+            posOrdem(raiz.esq);
+            posOrdem(raiz.dir);
+            System.out.print(raiz.dados + " ");
+        }
+    }
+
+    public int tamanho() {
+        return calcularTamanho(raiz);
+    }
+
+    public int calcularTamanho(Nodo raiz) {
+        if (raiz == null) {
+            return 0;
+        }
+        return 1 + calcularTamanho(raiz.esq) + calcularTamanho(raiz.dir);
+    }
 }
--- java/ArvoreBinaria/ArvoreMain.java
+++ java/ArvoreBinaria/ArvoreMain.java
@@ -1,7 +1,26 @@
 package ArvoreBinaria;
 
 public class ArvoreMain {
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
 
-    ArvoreBinaria arvore = new ArvoreBinaria();
-    // arvore.add(8);
+        arvore.add(8);
+        arvore.add(2);
+        arvore.add(6);
+        arvore.add(9);
+        arvore.add(12);
+        arvore.add(17);
+        arvore.add(4);
+        arvore.add(11);
+        arvore.add(3);
+        arvore.add(5);
+
+        arvore.emOrdemInicio();
+        System.out.println();
+        arvore.preOrdemInicio();
+        System.out.println();
+        arvore.posOrdemInicio();
+        System.out.println();
+        System.out.println(arvore.tamanho());
+    }
 }
